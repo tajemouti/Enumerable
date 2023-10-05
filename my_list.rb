60d42f007b9dd650072a95ce36b72642ad60ac16
tajemouti <tajemouti@gmail.com>
2023-10-05 16:11:52 +0100

    Implement the required change

--- my_list.rb
+++ my_list.rb
@@ -7,7 +7,14 @@ class MyList
     @list = list
   end
 
-  def each(&block)
-    @list.each(&block)
+  def each
+    return to_enum(:each) unless block_given?
+
+    counter = 0
+    while counter < @list.length
+      yield(@list[counter])
+      counter += 1
+    end
+    self
   end
 end
